b8a3ca13b7c2df3ad6e354b9b92cd7b4d9b6a38e
iriasyk <riasik123400@gmail.com>
2018-10-21 22:11:09 +0300

    Update script.js

--- JS3/functions-advanced/script.js
+++ JS3/functions-advanced/script.js
@@ -1,152 +1,102 @@
-// Task 1
-myname = "global";
-function func() {
-    console.log(myname); //undefined
-    var myname = "local";
-    console.log(myname); //local
+//  Task 1
+/*  Напишите фулнкцию которая будет выводить текст на екран: "Как ваше имя?";
+    После ввода имя запишите как свойство "name" для ссылки внутри функции this;
+
+    Создайте пустой объект и вызовите эту функцию для объекта и просто для окна и посмотрите изменился ли объект,
+    и создалась ли переменная name в окне браузера?
+    let Name = prompt('Как ваше имя?') ; */
+
+function inputName(){
+    return this.name = Name;
 }
-func(); //undefined
+// let empty = {};
+// empty.name = inputName(Name);
+
+window.inputName(Name);
+console.log(window.name);
 
 // Task 2
-var a = 90100;
-function someFunc(){
-    if(false){
-        var a = 1;
-    } else {
-        var b = 2;
+/* Напишите функцию sumTo(n), которая для данного n вычисляет сумму чисел от 1 до n. */
+
+function SumTo(n) {
+    result = 0;
+    for(var i = 1; i <= n; i++){
+        result += i;
     }
-    console.log(b); //2
-    console.log(a); //undefined
-}
-someFunc(); //undefined
-
-// Task 3
-a(); //ok
-b(); //error.Почему?: Потому, что функция вызываеться до обьявления.
-
-var b = function(){
-    alert( 'function b');
-}
-
-function a(){
-    alert( 'function a' );
-}
-
-//Task 4
-var i = 0;
-
-for (; i< 10; i ++) {
-    console.log(i) //1 2 3 4 5 6 7 8 9
-}
-
-//Task 5 (Напишите функцию подсчета факториала числа, но каждый раз возвращая факториал умножайте его на 2 если число четное.)
-
-function pow(value) {
-    let counter = value;
-    let result = 1;
-    if (value === 0) {
-        console.log(result)
-    }
-    while(value > 0) {
-        if (counter === 0){
-            break;
-        }
-        result *= counter;
-        counter = counter - 1;
-    }
-    if (value % 2 === 0) {
-        console.log(result*2)
-    }
-    else {
-        console.log(result)
-    }
-}
-pow(5);
-
-
-//Task 6
-//Подсчитайте время выполнения выше написанной вами функции c помощью объекта console.
-function factrl(counter) {
-    console.time("start");
-    var counter = prompt("Введите число:");
-    var factorial = 1;
-    document.write("Факториал числа: " + counter + "! = ");
-    while(counter > 0){
-        if (counter == 0){
-            factorial = 1;
-            break;
-        }
-        factorial *= counter;
-        counter = counter - 1;
-    }
-    console.timeEnd("start");
-    document.write(factorial);
-}
-console.log(factrl());
-
-//Task 7
-//Напишите скрипт, который генерирует случайную строку.
-//В строке должны быть маленькие и большие латинские буквы, цифры и подчеркивание.
-function random() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    return text;
-}
-
-console.log(random());
-
-//Task 8
-//Реализуйте функцию union, которая объединит уникальные элементы всех массивов, переданных ей параметром.
-//Пример: union([1, 2, 3], [101, 2, 1, 10], [2, 1]) вернёт [1, 2, 3, 101, 10].
-var arr1 = [1, 2, 3];
-var arr2 = [101, 2, 1, 10];
-var arr3 = [2,1];
-
-function union(){
-    var arr4 = [];
-    arr4 = [...new Set([...arr1 ,...arr2 ,...arr3])];
-    console.log(arr4);
-}
-union(arr1,arr2,arr3);
-
-//Task 9
-//Реализуйте функцию flatten, которая в случае,
-// если массив обладает уровнями вложенности, приведёт его к элементарному виду (вложенность может быть любой глубины).
-// Пример: flatten([1, [2], [3, [[[4]]]]]) вернёт [1, 2, 3, 4].
-
-function union(...arrs) {
-
-    let newArr = [];
-    for (let i in arrs) {
-        newArr = newArr.concat(arrs[i]);
-    }
-    let uniqueArr = Array.from(new Set(newArr));
-    console.log(uniqueArr);
-
-}
-union([1, 2, 3, 4], [1, 2, 3], [3, 4, 5, 6, 7, 15]);
-
-//Task 10
-//Реализуйте функцию unique, которая будет удалять дубли из массива.
-
-function unique(arr) {
-    var result = [];
-
-    nextInput:
-        for (var i = 0; i < arr.length; i++) {
-            var str = arr[i]; // для каждого элемента
-            for (var j = 0; j < result.length; j++) { // ищем, был ли он уже?
-                if (result[j] == str) continue nextInput; // если да, то следующий
-            }
-            result.push(str);
-        }
     return result;
 }
-var strings = ["one", "two", "three", "one",
-    "two", "three", 1, 1, 2
-];
-console.log( unique(strings) );
+console.log(SumTo(5));
+
+// Task 3
+/* Напишите функцию min(a,b), которая возвращает меньшее из чисел a,b. не используя Math.min(); */
+
+function min(a,b) {
+    if(a < b){
+        return a;
+    }else if(a > b){
+        return b;
+    }
+}
+console.log(min(3,5));
+
+//  Task 4
+/*  Создайте объект у которого будет:
+    свойство "number" которое не будет перечисляемым при цикле for .. in и будет равно 9
+    свойство "cantDelete" которое нельзя удалить
+    метод "getDouble" которое будет записывать число которое будет передаватся в метод как аргумент умноженное на 2;
+    а возвращать это же число деленное на 3; (то есть на set будет умножать число на 2,
+    а на get будет отдавать это же число деленное на 3 ) */
+
+// var object = {
+//     number: 9,
+//     cantDelete: 4,
+//     getDouble: function(n) {
+//         var result = n * 2;
+//         return result / 3;
+//     }
+// };
+// console.log(object.getDouble(3));
+let obj = {};
+
+Object.defineProperty(obj,'number',{
+    value: 9,
+    enumerable: true
+});
+Object.defineProperty(obj,'cantDelete',{
+    configurable: false
+});
+
+Object.defineProperty(obj, 'getDouble', {
+    get: function(n){return n*2;},
+    set: function(n){return n/3; },
+});
+
+// Task 5
+/* Напишите код, который отсортирует массив объектов people по полю age. */
+ let objs = [
+     { name: 'Вася', age: 23 },
+     { name: 'Маша', age: 18 },
+     { name: 'Вовочка', age: 6 }
+ ];
+
+ function compare(a,b) {
+     if (a.age < b.age)
+         return -1;
+     if (a.age > b.age)
+         return 1;
+     return 0;
+ }
+ console.log(objs.sort(compare));
+
+// Task 6
+/* Напишите функцию unique(arr), которая возвращает массив, содержащий только уникальные элементы arr (arr — массив строк). */
+var massive = ['2', 'abc', 'bva', 'ghj', 'bva', '1', '2', '1'];
+function unique(arr) {
+    let obj = {};
+    for (let i = 0; i < arr.length; i++){
+        let str = arr[i];
+        obj[str] = true;
+    }
+    return Object.keys(obj);
+}
+console.log(unique(massive));
